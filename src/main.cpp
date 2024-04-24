045db3259a93eb30ae059a57b1c4250dda3e68ff
MatixLoc <115873997+MatixLoc@users.noreply.github.com>
2024-04-24 17:23:07 +0200

    implement_Workflow (#2)
    * implement_Workflow
    
    * clang-format

--- src/main.cpp
+++ src/main.cpp
@@ -3,21 +3,17 @@
 /**
  * @brief A class that represents a minimal node.
  */
-class WaypointNode : public common_lib::CommonNode
-{
-public:
+class WaypointNode : public common_lib::CommonNode {
+   public:
     /**
      * @brief Constructs a new WaypointNode object.
      *
      * @param id The unique name for the node.
      */
-    WaypointNode() : CommonNode("waypoint_node")
-    {
-    }
+    WaypointNode() : CommonNode("waypoint_node") {}
 };
 
-int main(int argc, char *argv[])
-{
+int main(int argc, char *argv[]) {
     rclcpp::init(argc, argv);
     rclcpp::spin(std::make_shared<WaypointNode>());
     rclcpp::shutdown();
